feddfed8c0758909a518bebcf1c10410b65ff552
kingklass000 <momunovaman50@gmail.com>
2024-09-07 18:20:35 +0600

    update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -29,7 +29,7 @@ schema_view = get_schema_view(
         title="Online Store",
         default_version='v1',),
     public=True,
-    permission_classes=[permissions.AllowAny,],
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = i18n_patterns (
